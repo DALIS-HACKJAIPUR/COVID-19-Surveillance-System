aa63275aaff3ffc339b0b0a4a07f8e5c5bf33760
_ritik_ <ritikgarg1975@gmail.com>
2020-06-20 14:00:24 +0530

    twilio dummy api key

--- twilio.py
+++ twilio.py
@@ -2,7 +2,7 @@
 
 #to check, we need to install twilio ; pip install twilio
 from twilio.rest import Client #imported twilio Client
-acc_sid = "AC5c4ca1c5873e7cbfbafc50da59414f13" #dummy number acc_sid
-auth_token = "32ca306f7ddc1ec174f14b52423490a1" #dummy number auth_token
+acc_sid = "AC5c4ca1c5873e7cbfbafc5xxxxxxxx4f13" #dummy number acc_sid
+auth_token = "32ca306f7ddc1ec174f1xxxxxxxx90a1" #dummy number auth_token
 
 client = Client(acc_sid, auth_token) #creation of client
