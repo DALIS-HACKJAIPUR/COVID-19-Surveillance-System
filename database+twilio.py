a3a37ef48fd532111e43b4d7915bf472b440d642
_ritik_ <ritikgarg1975@gmail.com>
2020-06-20 17:46:43 +0530

    Added is_visited column in DB to keep track of messages, but need to add something which ensures is_visited column in tweaked only after 24hrs

--- database+twilio.py
+++ database+twilio.py
@@ -5,26 +5,28 @@ import sqlite3
 
 conn = sqlite3.connect('Entries.db') #making table Entries 
 c = conn.cursor() #created cursor to work with table
-c.execute("CREATE TABLE entries (first text, plate text, phone text, number_fine integer)") #creating columns in table for first name, no plate, phone number and number fine.
+c.execute("CREATE TABLE entries (first text, plate text, phone text, number_fine integer, is_visited integer)")
+#Adding column is_visited in order to make sure the person doesn't have to pay fine twice in same day.
 
 
 #Creating user class in order to add user efficiently
 class User():
 
-  def __init__(self, first, plate, phone, number_fine):
+  def __init__(self, first, plate, phone, number_fine, is_visited):
     self.first = first
     self.plate = plate
     self.phone = phone
     self.number_fine = number_fine
+    self.is_visited = is_visited
 
 #Dummy users
-user1 = User('Ritik', 'MP 09 BC1937', '9174333707', 0)
-user2 = User('Sourav', 'MH 02 PD1120', '8989209896', 0)
+user1 = User('Ritik', 'MP 09 BC1937', '9174333707', 0, 0) #initializing is_visited to 0
+user2 = User('Sourav', 'MH 02 PD1120', '8989209896', 0, 0)
 
 #Adding these dummy users into the database
 with conn:
-  c.execute("INSERT INTO Entries VALUES (:first, :plate, :phone, :number_fine)", {'first':user1.first, 'plate':user1.plate, 'phone':user1.phone, 'number_fine':user1.number_fine})
-  c.execute("INSERT INTO Entries VALUES (:first, :plate, :phone, :number_fine)", {'first':user2.first, 'plate':user2.plate, 'phone':user2.phone, 'number_fine':user2.number_fine})
+  c.execute("INSERT INTO Entries VALUES (:first, :plate, :phone, :number_fine, :is_visited)", {'first':user1.first, 'plate':user1.plate, 'phone':user1.phone, 'number_fine':user1.number_fine, 'is_visited':user1.is_visited})
+  c.execute("INSERT INTO Entries VALUES (:first, :plate, :phone, :number_fine, :is_visited)", {'first':user2.first, 'plate':user2.plate, 'phone':user2.phone, 'number_fine':user2.number_fine, 'is_visited':user2.is_visited})
 
 #Confirming addition of Users.
 with conn:
@@ -45,22 +47,42 @@ ph='9174333707'
 
 #opening DB
 with conn:
-  c.execute("SELECT number_fine FROM Entries WHERE phone=:phone", {'phone':ph}) #selecting number_fine column from row with dummy phone number
-  if((c.fetchall()[0][0])==0):
-    c.execute("UPDATE Entries SET number_fine = :number_fine WHERE phone = :phone",{'number_fine':1, 'phone':ph})
-    print("First Warning") #if this is the first time, he was caught, giving warning,
-     #Sending message with TWILIO API
+    c.execute("SELECT number_fine, is_visited FROM Entries WHERE phone=:phone", {'phone':ph}) #selecting both number_fine and is_visited from the database
+    li = c.fetchall()[0] #li is list of [number_fine, is_visited]
+    if(li[0]==0 and li[1]==0): #if both number_fine and is_visited are 0 
+    #both zero means person is being scanned for first time.
+
+    c.execute("UPDATE Entries SET number_fine = :number_fine, is_visited = :is_visited WHERE phone = :phone",{'number_fine':1, 'is_visited':1,'phone':ph})
+    print("First Warning")
+    #Twilio warning message
     client.messages.create(
                             to = "+91"+ph,
                             from_ = "+12513222811",
                             body = "This is your first warning, as you were not wearing a mask today while driving"
-                          )
-                         
-  else:
-    print("You have to give fine") #if number_fine is not 0, its more than first time he has broken
-    #Sending message with TWILIO API
+                            )
+
+    elif(li[0]!=0 and li[1]==0): #if number_fine is not 0 but is_visited is 0
+    #This situation indicates that a person has been warned before but not warned today, hence pay the fine.
+
+    c.execute("UPDATE Entries SET is_visited = :is_visited WHERE phone = :phone",{'is_visited':1,'phone':ph})
+    print("You have to give fine")
+    #Twilio fine message
     client.messages.create(
-                            to = "+91"+str(ph),
+                            to = "+91"+ph,
                             from_ = "+12513222811",
-                            body = "This is a message for you stating your fine, as you were not wearing a mask today while driving for a second or more than second time"
-                          )
+                            body = "This is a message for you stating your fine, as you were not wearing a mask today while driving even after a final warning"
+                            )
+
+    else: #if number fine is not 0 and is_visited is also not 0
+    #this situation corresponds the person has been encountered same day before. Hence *Final Warning*
+
+    c.execute("SELECT day FROM Entries WHERE phone=:phone", {'phone':ph})
+    c.execute("UPDATE Entries SET is_visited = :is_visited Where phone = :phone", {'is_visited':0, 'phone':ph})
+    print("No Mask found again today (Final Warning)")
+    #Twilio Final Warning Message
+    client.messages.create(
+                        to = "+91"+ph,
+                        from_ = "+12513222811",
+                        body = "This is your final warning, as you were not wearing a mask today while driving. You have to give fine after this."
+                        )
+    
