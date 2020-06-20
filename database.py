dd0516efc6a42fcd180b9a4d67c537ccf02759b8
_ritik_ <ritikgarg1975@gmail.com>
2020-06-20 13:28:29 +0530

    Added users to DB and executed the code

--- database.py
+++ database.py
@@ -18,3 +18,13 @@ class User():
 #Dummy users
 user1 = User('Ritik', 'MP 09 BC1937', '9174333707', 0)
 user2 = User('Sourav', 'MH 02 PD1120', '8989209896', 0)
+
+#Adding these dummy users into the database
+with conn:
+  c.execute("INSERT INTO Entries VALUES (:first, :plate, :phone, :number_fine)", {'first':user1.first, 'plate':user1.plate, 'phone':user1.phone, 'number_fine':user1.number_fine})
+  c.execute("INSERT INTO Entries VALUES (:first, :plate, :phone, :number_fine)", {'first':user2.first, 'plate':user2.plate, 'phone':user2.phone, 'number_fine':user2.number_fine})
+
+#Confirming addition of Users.
+with conn:
+  c.execute("SELECT * FROM Entries")
+  print(c.fetchall())
